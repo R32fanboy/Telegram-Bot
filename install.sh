9c29004f277545ec20006ea21d8a303365ab7295
R32fanboy <32889334+R32fanboy@users.noreply.github.com>
2019-09-30 09:47:54 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,26 +25,26 @@ echo    "#!/bin/bash
         do
                 sleep 1
                 timelimit -t5 telegram-cli -W -N --permanent-msg-ids > templog.txt
-                if grep -a "added by link" templog.txt | grep -a -F '$CHAT'; then
-                        grep -a "added by link" templog.txt | grep '$CHAT'> $SHORT.txt
+                if grep -a 'added by link' templog.txt | grep -a -F '$CHAT'; then
+                        grep -a 'added by link' templog.txt | grep '$CHAT'> $SHORT.txt
                         telegram-cli -W -e 'msg $CHAT "Hello new chat member. Please read any rules associated with this chat and know that your join has been logged. Enjoy your day."'
                         else echo not found
                 fi
-                if grep -a -F "$NAME" templog.txt | grep -a -F "$CHAT";  then
+                if grep -a -F '$NAME' templog.txt | grep -a -F '$CHAT';  then
                         telegram-cli -W -e 'msg $CHAT "I am online and Watching."'
                         else echo not found
                 fi
-                if grep -a -F "!log" templog.txt | grep -a -F '$CHAT' | grep -a -F '$ADMIN'; then
+                if grep -a -F '!log' templog.txt | grep -a -F '$CHAT' | grep -a -F '$ADMIN'; then
                         telegram-cli -W -e 'msg $CHAT "I am sending a log file your way. If it is empty or I do not send one, there is no log to send or view. After consulting this log, please purge at your own discression to ensure a clean log for the next event. I apologise for the extra mess as I do not yet know how to write in a completely human readable way."'
                         sleep 2
                         telegram-cli -W -e "post_file $CHAT $SHORT.txt"
                 fi
-                if grep -a -F "!purge" templog.txt | grep -a -F '$CHAT' | grep -a -F '$ADMIN'; then
+                if grep -a -F '!purge' templog.txt | grep -a -F '$CHAT' | grep -a -F '$ADMIN'; then
                         cp $SHORT.txt $SHORT.archive.txt
                         rm $SHORT.txt
                         telegram-cli -W -e "msg $CHAT 'The log has successfully been purged'"
                 fi
-                if grep -a -F "!source" templog.txt | grep -a -F '$CHAT'; then
+                if grep -a -F '!source' templog.txt | grep -a -F '$CHAT'; then
                         telegram-cli -W -e 'msg $CHAT "I am open source. You can check me out at https://github.com/R32fanboy/Telegram-Bot"'
                 fi
                 done" > script.sh
